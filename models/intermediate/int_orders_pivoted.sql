45e180eb367edd09e84314ca1b3a8cc3570dcc05
Kuenzang Dorji <kluckyd@gmail.com>
2026-06-20 13:13:39 +0000

    snowfalke_contract rates

--- models/intermediate/int_orders_pivoted.sql
+++ models/intermediate/int_orders_pivoted.sql
@@ -9,7 +9,7 @@ pivoted as
     select order_id,
         {% set payment_methods=['bank_transfer','coupon','credit_card','gift_card'] %}
             {% for payment_method in payment_methods %}
-                sum(case when payment_method={{payment_method}} then payment_amount    else 0 end) as {{payment_method}}_amount
+                sum(case when payment_method='{{payment_method}}' then payment_amount    else 0 end) as {{payment_method}}_amount
                 {%- if not loop.last -%} ,{%- endif -%}
             {% endfor %}
 
